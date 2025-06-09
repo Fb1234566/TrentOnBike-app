30e960f357047d9261cfa8365f69e6ad88a3ffec
Filippo Benedetti <filippo.benedetti3@gmail.com>
2025-06-09 19:55:05 +0200

    Aggiunta piattaforma android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.trentonbike.app',
+  appName: 'TrentOnBike',
+  webDir: 'dist'
+};
+
+export default config;
